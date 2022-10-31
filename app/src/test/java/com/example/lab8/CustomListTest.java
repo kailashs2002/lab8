e00b24e7c2af7c4425d4fa00da85355a5689c3bb
Kailash Seshadri <87671876+kailashs2002@users.noreply.github.com>
2022-10-31 16:04:48 -0600

    addCityTest fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,20 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    public City mockCity(){
+        return new City("Edmonton", "AB");
+    }
 
+    public CustomList mockCityList() {
+        return new CustomList(null, new ArrayList<>());
+    }
+
+
+    @Test
+    public void addCityTest(){
+        CustomList list1 = mockCityList();
+        int listSize = list1.getCount();
+        list1.addCity(mockCity());
+        assertEquals(list1.getCount(),listSize + 1);
+    }
 }
